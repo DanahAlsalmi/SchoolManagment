d41611ea60c60aa433cb641cd21e9fbd0ecc3728
Danah alsalmi <118121745+DanahAlsalmi@users.noreply.github.com>
2024-08-21 20:59:40 +0300

    Add files via upload

--- src/main/java/com/example/school/Advice/ControllerAdvice.java
+++ src/main/java/com/example/school/Advice/ControllerAdvice.java
@@ -1,61 +1,76 @@
 package com.example.school.Advice;
 
 import com.example.school.Api.ApiException;
-import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.UnexpectedTypeException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @org.springframework.web.bind.annotation.RestControllerAdvice
 public class ControllerAdvice {
+
     @ExceptionHandler(value = ApiException.class)
-    public ResponseEntity ApiResponse(ApiException ex){
-        String message = ex.getMessage();
-        return  ResponseEntity.status(400).body(message);
-    }
-    @ExceptionHandler(value = DataIntegrityViolationException.class) //duplicate
-    public ResponseEntity DataIntegrityViolationException(DataIntegrityViolationException ex){
-        String message = ex.getMessage();
-        return  ResponseEntity.status(400).body(message);
-    }
-    @ExceptionHandler(value = EntityNotFoundException.class) //ask for no exist id
-    public ResponseEntity EntityNotFoundException(EntityNotFoundException ex){
-        String message = ex.getMessage();
-        return  ResponseEntity.status(404).body(message);
-    }
-    @ExceptionHandler(value = HttpMessageNotReadableException.class) //wrong date
-    public ResponseEntity HttpMessageNotReadableException(HttpMessageNotReadableException ex){
-        String message = ex.getMessage();
-        return  ResponseEntity.status(400).body(message);
-    }
-    @ExceptionHandler(value = NoResourceFoundException.class) //get for no exist id //wrong path
-    public ResponseEntity NoResourceFoundException(NoResourceFoundException ex){
-        String message = ex.getMessage();
-        return  ResponseEntity.status(404).body(message);
-    }
-    @ExceptionHandler(value = IllegalArgumentException.class) //wrong date validate
-    public ResponseEntity IllegalArgumentException(IllegalArgumentException ex){
-        String message = ex.getMessage();
-        return  ResponseEntity.status(400).body(message);
-    }
-    @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class) //update for get but the right post
-    public ResponseEntity HttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex){
-        String message = ex.getMessage();
-        return  ResponseEntity.status(400).body(message);
-    }
-    @ExceptionHandler(value = MethodArgumentNotValidException.class) //wrong trnscationType
-    public ResponseEntity MethodArgumentNotValidException(MethodArgumentNotValidException ex){
-        String message = ex.getMessage();
-        return  ResponseEntity.status(400).body(message);
-    }
-    @ExceptionHandler(value = RuntimeException.class) //delete entity
-    public ResponseEntity RuntimeException(RuntimeException ex){
-        String message = ex.getMessage();
-        return  ResponseEntity.status(500).body(message);
+    public ResponseEntity ApiException(ApiException e) {
+        String message = e.getMessage();
+        return ResponseEntity.status(400).body(message);
     }
 
+    //Request method 'POST' is not supported
+    @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity HttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        String message = e.getMessage();
+        return ResponseEntity.status(405).body(message);
+    }
+
+    //No static resource
+    @ExceptionHandler(value = NoResourceFoundException.class)
+    public ResponseEntity NoResourceFoundException(NoResourceFoundException e) {
+        String message = e.getMessage();
+        return ResponseEntity.status(404).body(message);
+    }
+    //Duplicate
+    @ExceptionHandler(value = DataIntegrityViolationException.class)
+    public ResponseEntity DataIntegrityViolationException(DataIntegrityViolationException e) {
+        String message = e.getMessage();
+        return ResponseEntity.status(500).body(message);
+    }
+
+    //remove column in json
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity MethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String message = e.getMessage();
+        return ResponseEntity.status(400).body(message);
+    }
+
+    //JSON }
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public ResponseEntity HttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        String message = e.getMessage();
+        return ResponseEntity.status(400).body(message);
+    }
+
+    //Path (,) to required type
+    @ExceptionHandler(value = MethodArgumentTypeMismatchException.class)
+    public ResponseEntity MethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        String message = e.getMessage();
+        return ResponseEntity.status(400).body(message);
+    }
+
+    //No validator could be found for constraint
+    @ExceptionHandler(value = UnexpectedTypeException.class)
+    public ResponseEntity UnexpectedTypeException(UnexpectedTypeException e) {
+        String message = e.getMessage();
+        return ResponseEntity.status(400).body(message);
+    }
+
+    @ExceptionHandler(value = NullPointerException.class)
+    public ResponseEntity NullPointerException(NullPointerException e) {
+        String message = e.getMessage();
+        return ResponseEntity.status(400).body(message);
+    }
 }
